30baade8784cafd927d3e2c914ac91d1e5890da5
Sathyas91 <sathya@qaoncloud.com>
2023-03-13 18:39:49 +0530

    testrun13/3/2023

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,45 @@
 const { defineConfig } = require("cypress");
+const xlsx = require('node-xlsx').default;
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (on, config) => {
+  on('task',{parseXlsx({filePath})
+  {return
+    new promise((resolve,
+    reject)=>
+    {try
+      {
+        const jsonData = xlsx.parse(fs.readFileSync(filePath));
+        resolve(jsonData);
+      }
+       catch(e)
+      {
+        reject(e);
+      }});
+      
+    }});}
+    
+    const readXlsx = require('./cypress/support/read.xlsx')
+    module.exports = (on, config)=>{
+      on('task',{
+        'readXlsx':
+        readXlsx.read
+      })
+    }
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task',{
+        'readXlsx':
+        readXlsx.read
+})
     },
+
+    
   },
+  projectId: 's1tzhs',
+  "screenshotOnRunFailure":true,
 });
